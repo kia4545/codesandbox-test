0abe9843182ef9914bb11ed20d458da3aaf2d372
kia4545 <81552960+kia4545@users.noreply.github.com>
2021-09-16 23:09:10 +0900

    三項演算子 三項演算子の練習

--- src/index.js
+++ src/index.js
@@ -181,3 +181,21 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 //   }
 // })
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件 ? 条件がtrueの時: 条件がfalesの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力して下さい';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
+// }
+// console.log(checkSum(30, 60));
